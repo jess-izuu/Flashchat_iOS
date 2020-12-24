45e9a119521956e244793d17f0eaa708a3890471
jess-izuu <izumijessica@gmail.com>
2020-12-24 13:03:12 -0500

    Initial Commit

---
+++ Flashchat_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashchat_iOS
+//
+//  Created by Jessica Izumi on 12/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
