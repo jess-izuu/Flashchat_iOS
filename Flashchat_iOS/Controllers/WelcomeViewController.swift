b19748f5332e19f79a14eabe18da50e1e538c32b
jess-izuu <izumijessica@gmail.com>
2020-12-24 17:02:19 -0500

    WelcomeViewController

---
+++ Flashchat_iOS/Controllers/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Flashchat_iOS
+//
+//  Created by Jessica Izumi on 12/24/20.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
